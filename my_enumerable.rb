4b8d0bed0010de0286a3a7a1e6739f6918803d92
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-01-27 20:09:45 +0100

    Add all, any, and filter module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,3 +1,22 @@
 module MyEnumerable
-  def all; end
+  # All module
+  def all(&block)
+    all_result = @list.map(&block)
+    !all_result.include? false
+  end
+
+  # Any Module
+  def any(&block)
+    any_result = @list.map(&block)
+    !any_result.include? false
+  end
+
+  # Filter
+  def filter
+    filter = []
+    @list.each do |item|
+      filter.push(item) if yield(item)
+    end
+    filter
+  end
 end
--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,8 @@
+require_relative 'my_enumerable'
+
 class MyList
+  include MyEnumerable
+
   def initialize(*arg)
     @list = arg
   end
@@ -7,3 +11,11 @@ class MyList
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts (list.all { |i| i < 5 })
+puts (list.all { |i| i > 5 })
+puts (list.any { |i| i == 2 })
+puts (list.any { |i| i == 5 })
+p (list.filter { |i| i.even? })
